3eab27df6382e827f45a69d6af389a5a85ba75f4
Yuchi Tung <tracy770830@gmail.com>
2019-10-13 17:00:53 +0800

    refactor: 重構第九式：運用Design Pattern-策略模式

--- ShippingFee/Calculator.php
+++ ShippingFee/Calculator.php
@@ -1,14 +1,13 @@
 <?php
-include_once('BlackCat.php');
-include_once('Hsinchu.php');
-include_once('PostOffice.php');
-include_once ('Product.php');
+include_once ('BlackCat.php');
+include_once ('Hsinchu.php');
+include_once ('PostOffice.php');
+include_once('Product.php');
 
 /**
  * Class Calculator
- * 重構第八式：介面導向
- * 用該物件的角度去看世界，除了物件自己本身以外，看出去外面的世界，都是介面
- * ref: https://ithelp.ithome.com.tw/articles/10107028
+ * 重構第九式：運用Design Pattern-策略模式
+ * ref: https://ithelp.ithome.com.tw/articles/10107271
  */
 class Calculator
 {
@@ -25,47 +24,24 @@ class Calculator
 
     protected function calculate()
     {
-        //選黑貓，計算出運費，呈現物流商名稱與運費
-        if ($this->dropCompanyId === 1) {
-            /*
-            $blackCat = new BlackCat($this->product);
-            $blackCat->calculate();
-            $this->companyName = $blackCat->getCompanyName();
-            $this->charge = $blackCat->getCharge();
-            */
-
-            $logistics = new BlackCat($this->product);
+        $logistics = $this->getILogistics($this->dropCompanyId, $this->product);
+        if ($logistics !== null) {
             $logistics->calculate();
             $this->companyName = $logistics->getCompanyName();
             $this->charge = $logistics->getCharge();
+        }
+    }
 
-            //選新竹貨運，計算出運費，呈現物流商名稱與運費
-        } else if ($this->dropCompanyId === 2) {
-            /*
-            $hsinchu = new Hsinchu($this->product);
-            $hsinchu->calculate();
-            $this->companyName = $hsinchu->getCompanyName();
-            $this->charge = $hsinchu->getCharge();
-            */
-
-            $logistics = new Hsinchu($this->product);
-            $logistics->calculate();
-            $this->companyName = $logistics->getCompanyName();
-            $this->charge = $logistics->getCharge();
-
-            //選郵局，計算出運費，呈現物流商名稱與運費
-        } else if ($this->dropCompanyId === 3) {
-            /*
-            $postOffice = new PostOffice($this->product);
-            $postOffice->Calculate();
-            $this->companyName = $postOffice->getCompanyName();
-            $this->charge = $postOffice->getCharge();
-            */
-
-            $logistics = new PostOffice($this->product);
-            $logistics->calculate();
-            $this->companyName = $logistics->getCompanyName();
-            $this->charge = $logistics->getCharge();
+    private function getILogistics(int $companyId, Product $product)
+    {
+        if ($companyId === 1) {
+            return new BlackCat($product);
+        } else if ($companyId === 2) {
+            return new Hsinchu($product);
+        } else if ($companyId === 3) {
+            return new PostOffice($product);
+        } else {
+            return null;
         }
     }
 
