c6700e9ea586a2cd572b6a39da720bf9732d416e
Yuchi Tung <tracy770830@gmail.com>
2019-10-13 15:59:17 +0800

    refactor: 重構第三式：垃圾分類

--- ShippingFee/Calculator.php
+++ ShippingFee/Calculator.php
@@ -2,12 +2,12 @@
 
 /**
  * Class Calculator
- * 計算運費
- * 重構第二式：說人話
+ * 重構第三式：垃圾分類
  * ref: https://ithelp.ithome.com.tw/articles/10105320
  */
-class  Calculator
+class Calculator
 {
+    protected $isValid = true;
     protected $dropCompanyId = 0;
     protected $companyName = '';
     protected $charge = 0;
@@ -25,10 +25,13 @@ class  Calculator
         $this->weight = $weight;
     }
 
-    public function calculate()
+    protected function calculate()
     {
         //選黑貓，計算出運費，呈現物流商名稱與運費
         if ($this->dropCompanyId === 1) {
+            $this->calculatedByBlackCat();
+
+            /*
             $this->companyName = "黑貓";
             $weight = $this->weight;
             if ($this->weight > 20) {
@@ -37,9 +40,13 @@ class  Calculator
                 $fee = 100 + $weight * 10;
                 $this->charge = $fee;
             }
+            */
 
             //選新竹貨運，計算出運費，呈現物流商名稱與運費
         } else if ($this->dropCompanyId === 2) {
+            $this->calculatedByHsinchu();
+
+            /*
             $this->companyName = "新竹貨運";
             $length = $this->length;
             $width = $this->width;
@@ -52,9 +59,13 @@ class  Calculator
             } else {
                 $this->charge = $size * 0.0000353 * 1200;
             }
+            */
 
             //選郵局，計算出運費，呈現物流商名稱與運費
         } else if ($this->dropCompanyId === 3) {
+            $this->calculatedByPostOffice();
+
+            /*
             $this->companyName = "郵局";
             $weight = $this->weight;
             $feeByWeight = 80 + $weight * 10;
@@ -69,6 +80,54 @@ class  Calculator
             } else {
                 $this->charge = $feeBySize;
             }
+            */
+
+        }
+    }
+
+    protected function calculatedByBlackCat()
+    {
+        $this->companyName = "黑貓";
+        $weight = $this->weight;
+        if ($this->weight > 20) {
+            $this->charge = 500;
+        } else {
+            $fee = 100 + $weight * 10;
+            $this->charge = $fee;
+        }
+    }
+
+    protected function calculatedByHsinchu()
+    {
+        $this->companyName = "新竹貨運";
+        $length = $this->length;
+        $width = $this->width;
+        $height = $this->height;
+        $size = $length * $width * $height;
+
+        //長 x 寬 x 高（公分）x 0.0000353
+        if ($length > 100 || $width > 100 || $height > 100) {
+            $this->charge = $size * 0.0000353 * 1100 + 500;
+        } else {
+            $this->charge = $size * 0.0000353 * 1200;
+        }
+    }
+
+    protected function calculatedByPostOffice()
+    {
+        $this->companyName = "郵局";
+        $weight = $this->weight;
+        $feeByWeight = 80 + $weight * 10;
+        $length = $this->length;
+        $width = $this->width;
+        $height = $this->height;
+        $size = $length * $width * $height;
+        $feeBySize = $size * 0.0000353 * 1100;
+
+        if ($feeByWeight < $feeBySize) {
+            $this->charge = $feeByWeight;
+        } else {
+            $this->charge = $feeBySize;
         }
     }
 
